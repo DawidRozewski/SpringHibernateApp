245c20f03f8c7409a61d12edf443e484444059b9
DawidRozewski <dawid.rrozewski@gmail.com>
2021-10-23 10:31:35 +0200

    Added `AppConfig` class.

---
+++ src/main/java/pl/coderslab/SpringHibernateApp/AppConfig.java
@@ -0,0 +1,12 @@
+package pl.coderslab.SpringHibernateApp;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = "pl.coderslab")
+@EnableTransactionManagement
+public class AppConfig {
+}
