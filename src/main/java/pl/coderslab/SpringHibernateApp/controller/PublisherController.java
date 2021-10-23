6cbbde3d01bd5f3ff8f1fc5380553d620acb884f
DawidRozewski <dawid.rrozewski@gmail.com>
2021-10-23 13:50:12 +0200

    Added `PublisherController` class in `controller` package and implemented methods.

---
+++ src/main/java/pl/coderslab/SpringHibernateApp/controller/PublisherController.java
@@ -0,0 +1,51 @@
+package pl.coderslab.SpringHibernateApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.coderslab.SpringHibernateApp.dao.PublisherDao;
+import pl.coderslab.SpringHibernateApp.entity.Publisher;
+
+public class PublisherController {
+
+
+    private final PublisherDao publisherDao;
+
+    public PublisherController(PublisherDao publisherDao) {
+        this.publisherDao = publisherDao;
+    }
+
+    @GetMapping("/save")
+    @ResponseBody
+    public String persist() {
+        Publisher publisher = new Publisher();
+        publisher.setName("PWN");
+        publisherDao.persist(publisher);
+        return publisher.toString();
+    }
+
+    @GetMapping("/find/{id}")
+    @ResponseBody
+    public String findById(@PathVariable long id) {
+        Publisher publisher = publisherDao.findById(id);
+        return publisher.toString();
+    }
+
+    @GetMapping("/update/{id}/{name}")
+    @ResponseBody
+    public String merge(@PathVariable long id, @PathVariable String name) {
+        Publisher publisher = publisherDao.findById(id);
+        publisher.setName(name);
+        publisherDao.merge(publisher);
+        return publisher.toString();
+    }
+
+    @GetMapping("/remove/{id}")
+    @ResponseBody
+    public String remove(@PathVariable long id) {
+        publisherDao.remove(id);
+        return "Usunieto wydawce";
+    }
+
+}
