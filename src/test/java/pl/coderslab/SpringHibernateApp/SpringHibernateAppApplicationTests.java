944bb10653b4aa9d847f6fa6a87faba58e6a2d62
DawidRozewski <dawid.rrozewski@gmail.com>
2021-10-23 10:06:44 +0200

    first commit

---
+++ src/test/java/pl/coderslab/SpringHibernateApp/SpringHibernateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.SpringHibernateApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
