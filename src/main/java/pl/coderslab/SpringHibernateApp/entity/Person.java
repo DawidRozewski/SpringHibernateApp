3e32a42a576f4794344e32d37d482f295215390c
DawidRozewski <dawid.rrozewski@gmail.com>
2021-10-23 15:40:47 +0200

    Added `Person` class in `entity` package.

---
+++ src/main/java/pl/coderslab/SpringHibernateApp/entity/Person.java
@@ -0,0 +1,27 @@
+package pl.coderslab.SpringHibernateApp.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+    private String email;
+
+
+}
