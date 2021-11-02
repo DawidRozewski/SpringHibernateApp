a9b689b93516350636ddb9b1cb27c2a54736eb9e
DawidRozewski <dawid.rrozewski@gmail.com>
2021-11-02 17:32:35 +0100

    Full CRUD `AuthorFormController`.

--- src/main/java/pl/coderslab/SpringHibernateApp/controller/AuthorFormController.java
+++ src/main/java/pl/coderslab/SpringHibernateApp/controller/AuthorFormController.java
@@ -2,9 +2,9 @@ package pl.coderslab.SpringHibernateApp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.coderslab.SpringHibernateApp.dao.AuthorDao;
+import pl.coderslab.SpringHibernateApp.entity.Author;
 
 @Controller
 @RequestMapping("/author/form")
@@ -20,6 +20,44 @@ public class AuthorFormController {
     public String all(Model model) {
         model.addAttribute("authors", authorDao.findAll());
         return "/author/authorListing";
+    }
+
+    @GetMapping("/add")
+    public String prepareAdd(Model model) {
+        model.addAttribute("author", new Author());
+        return "/author/authorForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@ModelAttribute("author") Author author) {
+        authorDao.persist(author);
+        return "redirect:/author/form/all";
+    }
+
+    @GetMapping("/edit")
+    public String prepareEdit(@RequestParam long id, Model model) {
+        model.addAttribute("author", authorDao.findById(id));
+        return "/author/authorForm";
+    }
+
+    @PostMapping("/edit")
+    public String merge(@ModelAttribute("author") Author author) {
+        authorDao.merge(author);
+        return "redirect:/author/form/all";
+    }
+
+    @GetMapping("/remove")
+    public String prepareRemove(@RequestParam long id, Model model) {
+        model.addAttribute("author", authorDao.findById(id));
+        return "/author/remove";
+    }
+
+    @PostMapping("/remove")
+    public String remove(@RequestParam String confirmed, @RequestParam long id) {
+        if ("yes".equals(confirmed)) {
+            authorDao.remove(id);
+        }
+        return "redirect:/author/form/all";
     }
 
 }
