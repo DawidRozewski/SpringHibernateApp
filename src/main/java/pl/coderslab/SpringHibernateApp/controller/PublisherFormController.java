2791ff8c497687893f7a9bd601c8dcf1e079ca38
DawidRozewski <dawid.rrozewski@gmail.com>
2021-11-02 18:18:20 +0100

    Full CRUD for `PublisherFormController`.

---
+++ src/main/java/pl/coderslab/SpringHibernateApp/controller/PublisherFormController.java
@@ -0,0 +1,64 @@
+package pl.coderslab.SpringHibernateApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.SpringHibernateApp.dao.PublisherDao;
+import pl.coderslab.SpringHibernateApp.entity.Publisher;
+
+@Controller
+@RequestMapping("/publisher/form")
+public class PublisherFormController {
+
+    private final PublisherDao publisherDao;
+
+    public PublisherFormController(PublisherDao publisherDao) {
+        this.publisherDao = publisherDao;
+    }
+
+    @GetMapping("/all")
+    public String all(Model model) {
+        model.addAttribute("publishers", publisherDao.findAll());
+        return "publisher/publisherListing";
+    }
+
+    @GetMapping("/add")
+    public String prepareToAdd(Model model) {
+        model.addAttribute("publisher", new Publisher());
+        return "publisher/publisherForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@ModelAttribute("publisher") Publisher publisher) {
+        publisherDao.persist(publisher);
+        return "redirect:/publisher/form/all";
+    }
+
+    @GetMapping("/edit")
+    public String prepareToEdit(@RequestParam long id, Model model) {
+        model.addAttribute("publisher", publisherDao.findById(id));
+        return "publisher/publisherForm";
+    }
+
+    @PostMapping("/edit")
+    public String merge(@ModelAttribute("publisher") Publisher publisher) {
+        publisherDao.merge(publisher);
+        return "redirect:/publisher/form/all";
+    }
+
+    @GetMapping("/remove")
+    public String prepareToRemove(@RequestParam long id, Model model) {
+        model.addAttribute("publisher", publisherDao.findById(id));
+        return "publisher/remove";
+    }
+
+    @PostMapping("/remove")
+    public String remove(@RequestParam String confirmed, @RequestParam long id) {
+        if ("yes".equals(confirmed)) {
+            publisherDao.remove(id);
+        }
+        return "redirect:/publisher/form/all";
+    }
+
+
+}
